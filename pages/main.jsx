b4c5e76534e2c4859dc4c193ce8ae4f7fb887ed7
Diego Moreno <iam@diegomoreno.co>
2022-10-09 03:13:29 -0500

    Layout craeted

---
+++ pages/main.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+const Main = () => {
+  const Layout = () => {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Main;
