face0bf296a5a14e2e5d537e573898df5d6f4391
Diego Moreno <iam@diegomoreno.co>
2022-10-09 00:29:25 -0500

    Redirect to Auth Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,32 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { useRouter } from "next/router";
 
-function LandingPage() {
+const LandingPage = () => {
+  const router = useRouter();
+
+  const goToAuth = () => {
+    router.push(`/auth`);
+  };
+
   return (
     <div style={{ backgroundColor: "black" }} className="vh-100">
-      <div style={{ backgroundColor: "black" }} className="container-fluid d-flex justify-content-center align-items-center h-100">
+      <div
+        style={{ backgroundColor: "black" }}
+        className="container-fluid d-flex justify-content-center align-items-center h-100"
+      >
         <div className="row">
           <div className="col">
             <div>
               <img src="/logo.jpeg" alt="Denetwork" />
             </div>
-            <button type="button" class="btn btn-dark mx-auto d-block">Let's start!</button>
+            <button type="button" class="btn btn-dark mx-auto d-block" onClick={() => goToAuth()}>
+              Let's start!
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default LandingPage;
