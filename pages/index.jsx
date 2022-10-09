b5db034f8a45953cd848cf467a811b6fe9e1a36a
Diego Moreno <iam@diegomoreno.co>
2022-10-09 00:18:59 -0500

    Simple landing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,20 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 function LandingPage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div style={{ backgroundColor: "black" }} className="vh-100">
+      <div style={{ backgroundColor: "black" }} className="container-fluid d-flex justify-content-center align-items-center h-100">
+        <div className="row">
+          <div className="col">
+            <div>
+              <img src="/logo.jpeg" alt="Denetwork" />
+            </div>
+            <button type="button" class="btn btn-dark mx-auto d-block">Let's start!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
